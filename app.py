4c1676991eb85a235d4701e8923d0e2840cd1689
Justin <jmoua1@student.sierracollege.edu>
2020-11-04 23:26:36 -0800

    added html form

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-import flask
+from flask import Flask, render_template, request
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "<h1>OscarProj</h1><p>This was created with an empty flask skeleton.</p>"
+    if request.method == "GET":
+        return render_template('home.html')
+    elif request.method =="POST":
+        return render_template('home.html', selection=request.form['rad'])
 
+@app.route('/display', methods=['GET', 'POST'])
+def display():
+    return render_template('display.html', selection=request.form['rad'])
 
 if __name__ == '__main__':
     app.run()
